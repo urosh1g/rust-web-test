19d60ccc4f322e8bd90ef87b3e775a135ba3e0e2
Urosh <urosh_h@protonmail.com>
2023-07-12 22:59:46 +0200

    comment model & article routes

---
+++ src/models/comment.rs
@@ -0,0 +1,19 @@
+#[derive(sqlx::FromRow, serde::Serialize, serde::Deserialize)]
+pub struct Comment {
+    pub id: i32,
+    pub user_id: i32,    // TODO user: User
+    pub article_id: i32, //      article: Article
+    pub content: String,
+}
+
+#[derive(sqlx::FromRow, serde::Serialize, serde::Deserialize)]
+pub struct NewComment {
+    pub user_id: i32,
+    pub article_id: i32,
+    pub content: String,
+}
+
+#[derive(sqlx::FromRow, serde::Serialize, serde::Deserialize)]
+pub struct UpdateComment {
+    pub content: String,
+}
--- src/routes/articles.rs
+++ src/routes/articles.rs
@@ -2,21 +2,66 @@ use crate::{
     models::article::{Article, NewArticle, UpdateArticle},
     AppState,
 };
-use actix_web::{delete, get, post, put, web, web::Path, HttpResponse, Responder};
+use actix_web::{
+    delete, get, post, put,
+    web::{self, Path},
+    HttpResponse, Responder,
+};
+
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    let scope = web::scope("/articles")
+        .service(get_articles)
+        .service(add_article)
+        .service(get_article)
+        .service(update_article)
+        .service(delete_article);
+    cfg.service(scope);
+}
 
 #[get("")]
 pub async fn get_articles(app_data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok()
+    let res = sqlx::query_as!(Article, "select * from articles")
+        .fetch_all(&app_data.db_pool)
+        .await;
+    match res {
+        Ok(vec) => HttpResponse::Ok().json(vec),
+        Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
+    }
 }
 
 #[post("")]
-pub async fn add_article(app_data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn add_article(
+    body: web::Json<NewArticle>,
+    app_data: web::Data<AppState>,
+) -> impl Responder {
+    //TODO proveri da li autor postoji
+    let res = sqlx::query!(
+        "insert into articles ( author_id, title, content ) values ( $1, $2, $3 )",
+        body.author_id,
+        body.title,
+        body.content
+    )
+    .execute(&app_data.db_pool)
+    .await;
+    match res {
+        Ok(_) => HttpResponse::Created(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
 }
 
 #[get("{article_id}")]
 pub async fn get_article(path: Path<i32>, app_data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok()
+    let article_id = path.into_inner();
+    let res = sqlx::query_as!(Article, "select * from articles where id = $1", article_id)
+        .fetch_optional(&app_data.db_pool)
+        .await;
+    match res {
+        Ok(optional) => match optional {
+            Some(article) => HttpResponse::Ok().json(article),
+            None => HttpResponse::NotFound().json("article not found"),
+        },
+        Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
+    }
 }
 
 #[put("{article_id}")]
@@ -27,14 +72,4 @@ pub async fn update_article(path: Path<i32>, app_data: web::Data<AppState>) -> i
 #[delete("{article_id}")]
 pub async fn delete_article(path: Path<i32>, app_data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok()
-}
-
-pub fn configure(cfg: &mut web::ServiceConfig) {
-    let scope = web::scope("/articles")
-        .service(get_articles)
-        .service(add_article)
-        .service(get_article)
-        .service(update_article)
-        .service(delete_article);
-    cfg.service(scope);
 }
