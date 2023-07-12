6fdd7da4e736ca597940b5328069359d7582a101
Urosh <urosh_h@protonmail.com>
2023-07-12 10:47:57 +0200

    database connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use actix_web;
 use dotenv::dotenv;
+use sqlx::{
+    postgres::{PgPoolOptions, Postgres},
+    Pool,
+};
 use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db_config = read_configuration();
+    println!("Successfully read the .env config");
+    let _ = db_connect(db_config).await;
+    println!("Successfully connected to the database");
+    Ok(())
+}
 
 struct DbConfig {
     connection_string: String,
@@ -20,7 +34,10 @@ fn read_configuration() -> DbConfig {
     }
 }
 
-fn main() {
-    let db_config = read_configuration();
-    println!("Successfully read the .env config");
+async fn db_connect(cfg: DbConfig) -> Pool<Postgres> {
+    PgPoolOptions::new()
+        .max_connections(cfg.max_connections)
+        .connect(&cfg.connection_string)
+        .await
+        .expect("Should be able to connect to db")
 }
